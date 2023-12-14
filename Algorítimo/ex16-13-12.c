ef546e8fed797060bdbdbdb0a05a0074af052fd5
danielgutierrez87 <daniell.mx@gmail.com>
2023-12-13 21:29:57 -0300

    novo exercício

---
+++ Algorítimo/ex16-13-12.c
@@ -0,0 +1,24 @@
+/* Fazer um programa que leia um número indeterminado de dados contendo
+a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém 
+o valor da idade igual a zero. Calcule e escreva a idade média deste grupo de indivíduos. */ 
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    
+    int idade=1, individuos=0, somaIdades=0;
+    float media=0.0;
+        while (idade>0) {
+            printf("Digite a idade de um indivíduo: ");
+                scanf("%i", &idade);
+                if (idade != 0) { 
+                    individuos++; 
+                }
+        somaIdades = somaIdades+idade;
+        }        
+        
+            media = somaIdades/individuos;
+            printf("A média de idade deste grupo é %f anos.", media);
+               
+    return 0;
+}
